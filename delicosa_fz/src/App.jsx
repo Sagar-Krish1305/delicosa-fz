e9a8ca908cf7f3dc9c2c37b1fcdf9751e8402d9c
SagarKrish1305 <sagarkrish1305@gmail.com>
2024-06-04 21:56:52 +0530

    Adding the Our Products Page

--- delicosa_fz/src/App.jsx
+++ delicosa_fz/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import React from 'react'
 import ContactUsForm from './Components/Contact Us Form/ContactUsForm'
+import ProductPage from './Pages/Our_Products/OurProductsPage'
+import ProductPageIntroduction from './Components/Our_Product_Introduction/ProductIntroduction'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div style={{backgroundColor: '#1F603C', height: '800vh'}}>
-      <ContactUsForm></ContactUsForm>
+    <div style={{backgroundColor: '#1F603C', height: '100%'}}>
+      <ProductPage selected={"Yellow"} />
     </div>
   )
 }
